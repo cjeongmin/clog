6f5121e344777f642049aa9e91b4d0f73a93310a
cjeongmin <cjeongmin27@gmail.com>
2022-09-05 22:35:20 +0900

    Refactor: Post 화면 스타일 추가

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -3,7 +3,7 @@ import { PostType } from "../../atoms/postListState";
 import { getPost } from "../../utils/firebase";
 
 const Body = ({ body }: { body: string }) => {
-  return <div dangerouslySetInnerHTML={{ __html: body }}></div>;
+  return <div dangerouslySetInnerHTML={{ __html: body }} />;
 };
 
 const Post = ({ title, body, date }: PostType) => {
@@ -12,16 +12,36 @@ const Post = ({ title, body, date }: PostType) => {
       <style jsx>{`
         .post {
           display: flex;
-          min-height: 100%;
+          min-height: 90%;
           flex-direction: column;
+          margin: 5%;
+          padding: 5%;
+          box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+        }
+
+        .head {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+        }
+
+        .head > h1 {
+          margin: 0 auto 0 auto;
+        }
+
+        hr {
+          width: 100%;
         }
       `}</style>
 
-      <div className="post">
-        <h1>{title}</h1>
-        <h5>{date}</h5>
+      <article className="post">
+        <div className="head">
+          <h1>{title}</h1>
+          <h5>{date}</h5>
+        </div>
+        <hr />
         <Body body={body} />
-      </div>
+      </article>
     </>
   );
 };
