169d231aeea7dc65f44a18111f0d78a4746dc3d4
cjeongmin <cjeongmin27@gmail.com>
2022-09-07 20:36:03 +0900

    Feat: editor 스타일 수정

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -34,7 +34,8 @@ const Edit = ({
           padding: 2%;
           box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
             rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-          height: 96%;
+          height: 98%;
+          overflow-y: auto;
         }
       `}</style>
 
