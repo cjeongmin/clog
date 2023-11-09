fc9118e70a8d0101ba9e06e1854fc04ca1330981
cjeongmin <cjeongmin27@gmail.com>
2023-11-09 15:18:55 +0900

    fix: API path.resolve 추가

--- src/app/api/posts/[name]/route.ts
+++
@@ -1,5 +0,0 @@
-import { trimFileName } from "@/libs/post";
-import MarkDownFile from "@/models/MarkDownFile";
-import fs from "fs";
-import { glob } from "glob";
-import { NextRequest, NextResponse } from "next/server";
