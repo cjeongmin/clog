11dc3fab6d69d01e718347e82b504e32b56ad2f2
cjeongmin <cjeongmin27@gmail.com>
2025-01-28 23:36:58 +0900

    feat: add zustand for state management and update tailwind config paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,11 @@ import typography from '@tailwindcss/typography';
 
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/page/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/widget/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/feature/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/entity/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
