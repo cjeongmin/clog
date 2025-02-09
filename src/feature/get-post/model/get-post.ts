4bcf02ae176161161181dc4e0ccda09c8156e4a7
cjeongmin <cjeongmin27@gmail.com>
2025-02-09 18:57:45 +0900

    feat: return null for draft posts in getPost function

--- src/feature/get-post/model/get-post.ts
+++ src/feature/get-post/model/get-post.ts
@@ -4,5 +4,6 @@ import { parsePostFile } from '@/entity/post';
 
 export const getPost = (fileName: string) => {
   const filePath = path.join(process.cwd(), 'post', `${decodeURIComponent(fileName)}.md`);
-  return parsePostFile(filePath);
+  const result = parsePostFile(filePath);
+  return result.draft ? null : result;
 };
