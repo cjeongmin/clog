8b37d897f2a3df6c80684cff086d40b04a992932
cjeongmin <cjeongmin27@gmail.com>
2025-07-25 00:56:04 +0900

    fix: fix command palette error

--- src/feature/command-palette/ui/CommandPalette.tsx
+++ src/feature/command-palette/ui/CommandPalette.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { IoIosSearch } from 'react-icons/io';
 import { useRouter } from 'next/navigation';
 
@@ -17,6 +18,11 @@ export default function CommandPalette({ posts }: Readonly<CommandPaletteProps>)
   const inputRef = useRef<HTMLInputElement>(null);
   const [visible, setVisible] = useState(false);
   const [query, setQuery] = useState('');
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const lowercasedQuery = query.toLowerCase();
 
@@ -65,58 +71,61 @@ export default function CommandPalette({ posts }: Readonly<CommandPaletteProps>)
       <button onClick={toggleVisible}>
         <IoIosSearch size={20} />
       </button>
-      {visible && (
-        <div className='fixed inset-0 z-50 flex items-center justify-center bg-overlay' onClick={toggleVisible}>
-          <div
-            className='flex h-[50dvh] w-[80dvw] min-w-[340px] max-w-[540px] flex-col gap-4 overflow-auto rounded-md bg-surface-primary p-4 sm:w-[30dvw]'
-            onClick={(event) => event.stopPropagation()}
-          >
-            <div className='flex flex-row items-center border-b border-border-primary'>
-              <IoIosSearch size={20} />
-              <input
-                ref={inputRef}
-                type='text'
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                className='w-full bg-inherit p-2 text-text-secondary focus:outline-none'
-              />
-            </div>
-            <div className='flex flex-row items-center gap-1 text-sm'>
-              <p>게시글</p>
-              <p className='text-text-muted'>{filteredPosts.length}</p>
-            </div>
-            <ul className='flex w-full flex-1 flex-col gap-2 overflow-y-auto'>
-              {filteredPosts.map((post) => (
-                <button
-                  onClick={() => {
-                    router.push(`/posts/${post.fileName}`);
-                    toggleVisible();
-                  }}
-                  key={post.title}
-                  className='flex w-full shrink-0 flex-row items-end gap-2 overflow-x-auto rounded-md border border-border-primary p-3 sm:transition-all sm:hover:border-border-interactive-hover'
-                >
-                  <PostItem post={post}>
-                    <div className='flex flex-col gap-2'>
-                      <div className='flex w-full flex-row items-center gap-2'>
-                        <PostItem.Title className='truncate text-base text-text-secondary' />
-                        <PostItem.Date className='whitespace-nowrap text-sm text-text-muted' />
+      {visible &&
+        mounted &&
+        createPortal(
+          <div className='fixed inset-0 z-50 flex items-center justify-center bg-overlay' onClick={toggleVisible}>
+            <div
+              className='flex h-[50dvh] w-[80dvw] min-w-[340px] max-w-[540px] flex-col gap-4 overflow-auto rounded-md bg-surface-primary p-4 sm:w-[30dvw]'
+              onClick={(event) => event.stopPropagation()}
+            >
+              <div className='flex flex-row items-center border-b border-border-primary'>
+                <IoIosSearch size={20} />
+                <input
+                  ref={inputRef}
+                  type='text'
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  className='w-full bg-inherit p-2 text-text-secondary focus:outline-none'
+                />
+              </div>
+              <div className='flex flex-row items-center gap-1 text-sm'>
+                <p>게시글</p>
+                <p className='text-text-muted'>{filteredPosts.length}</p>
+              </div>
+              <ul className='flex w-full flex-1 flex-col gap-2 overflow-y-auto'>
+                {filteredPosts.map((post) => (
+                  <button
+                    onClick={() => {
+                      router.push(`/posts/${post.fileName}`);
+                      toggleVisible();
+                    }}
+                    key={post.title}
+                    className='flex w-full shrink-0 flex-row items-end gap-2 overflow-x-auto rounded-md border border-border-primary p-3 sm:transition-all sm:hover:border-border-interactive-hover'
+                  >
+                    <PostItem post={post}>
+                      <div className='flex flex-col gap-2'>
+                        <div className='flex w-full flex-row items-center gap-2'>
+                          <PostItem.Title className='truncate text-base text-text-secondary' />
+                          <PostItem.Date className='whitespace-nowrap text-sm text-text-muted' />
+                        </div>
+                        <PostItem.Tags />
                       </div>
-                      <PostItem.Tags />
-                    </div>
-                  </PostItem>
-                </button>
-              ))}
-            </ul>
-            <footer className='flex self-end'>
-              <p className='hidden text-sm text-text-muted sm:block'>
-                {navigator.userAgent.toLowerCase().includes('mac')
-                  ? '⌘ K to toggle palette'
-                  : 'Ctrl K to toggle palette'}
-              </p>
-            </footer>
-          </div>
-        </div>
-      )}
+                    </PostItem>
+                  </button>
+                ))}
+              </ul>
+              <footer className='flex self-end'>
+                <p className='hidden text-sm text-text-muted sm:block'>
+                  {navigator.userAgent.toLowerCase().includes('mac')
+                    ? '⌘ K to toggle palette'
+                    : 'Ctrl K to toggle palette'}
+                </p>
+              </footer>
+            </div>
+          </div>,
+          document.body,
+        )}
     </>
   );
 }
